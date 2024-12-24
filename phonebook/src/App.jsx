c0009317e1ddd5327038b3666df43b3be61f1f6c
mtyler0606 <mtyler0606@gmail.com>
2024-12-24 08:17:18 -0600

    2.9

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -13,10 +13,15 @@ const Number = ({name, number}) => <p>{name} {number}</p>
 const App = () => {
   const [persons, setPersons] = useState([
     /* */
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const [isFiltered, setIsFiltered ] = useState(false)
   
   const submitHandler = (event) => {
     event.preventDefault()
@@ -37,9 +42,34 @@ const App = () => {
     setNumber("")
   }
 
+  const filterHandler = (event) => {
+    const newFilter = event.target.value;
+    setFilter(newFilter)
+    if(newFilter.length > 0){
+      setIsFiltered(true)
+    } else {
+      setIsFiltered(false)
+    }
+  }
+
+  const displayFiltered = () => {
+    if(!isFiltered){
+      return persons.map(person => <Number key={person.name} name={person.name} number={person.number} />)
+    }
+    else {
+      const filteredPersons = persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()))
+      return filteredPersons.map(person => <Number key={person.name} name={person.name} number={person.number} />)
+    }
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter show with
+        <input onChange={filterHandler} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={submitHandler}>
         <div>
           name: <input
@@ -59,7 +89,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <Number key={person.name} name={person.name} number={person.number} />)}
+        {displayFiltered()}
       </div>
     </div>
   )
